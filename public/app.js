6b1fb233211cf6893426ff0b0cee165db8b05c79
Justin Acosta <justinacosta@Justins-Air.fios-router.home>
2021-03-25 10:14:18 -0400

    saves to database

--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 
+// transfer all info to env
+var firebaseConfig = {
+    apiKey: "AIzaSyAu0pNoEMxSd7QcZqFSLJ1ZBqbUrRdRJ3Q",
+    authDomain: "lifestories-9f02d.firebaseapp.com",
+    projectId: "lifestories-9f02d",
+    storageBucket: "lifestories-9f02d.appspot.com",
+    messagingSenderId: "1021556811843" ,
+    appId: "1:1021556811843:web:838bb4a566fabe98108cf6",
+    measurementId: "G-6DVRJ3KRP8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var storyData = firebase.database();
+
 // receive entries
 function submitEntry() {
 
@@ -10,10 +26,8 @@ function submitEntry() {
 
     // story age
     let age = document.getElementById("age").value;
-
     // email
     let email = document.getElementById("email").value;
-    
     // story
     let story = document.getElementById("story").value.trim();
     
@@ -26,29 +40,45 @@ function submitEntry() {
             repeat+= "Please enter text!\n";        
         }
         document.getElementById("story").value = repeat;
-
         setTimeout(function() {document.getElementById("story").value = ""}, 3000);
     }
 
     else{   
+
+        if (email === "") {
+            if (age === "Age") {
+                age = "Adult";
+            }
+                let newStory = {
+                    lifeStory : {
+                        age: age,
+                        story: story
+                    }
+                }
+                storyData.ref().push(newStory);
+        }
+
+        else {
+            if (age === "Age") {
+                age = "Adult";
+            }
+
+            let newStory = {
+                lifeStory: {
+                    age: age,
+                    story: story
+                },
+                email: email
+            };
+            storyData.ref().push(newStory);
+        }
+
         valueReset();
         let thankYou = "";
         for (var i = 0; i < 24; i++) {
             thankYou+= "Thank you for your submission!\n";
         }
         document.getElementById("story").value = thankYou;
-        
-        for(var i = 0; i < 3; i++) {
-            if (i === 0) {
-                Submission.age = (age);
-            }
-            else if(i === 1) {
-                Submission.email = (email);
-            }
-            else {
-                Submission.story = (story);
-            }
-        }
     }
     
 }
